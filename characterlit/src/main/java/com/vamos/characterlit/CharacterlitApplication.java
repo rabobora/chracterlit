0f8671ee6e114c4c0a3e631b6787d7f06dd3b8f7
woojin <qpqpqp0102@naver.com>
2024-03-15 16:10:38 +0900

    Chore: 백 스켈레톤 코드

---
+++ characterlit/src/main/java/com/vamos/characterlit/CharacterlitApplication.java
@@ -0,0 +1,13 @@
+package com.vamos.characterlit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterlitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterlitApplication.class, args);
+	}
+
+}
