7d148e87f72da564fe6d7471f85528a8f44fd3b8
azin Lee <nuestiloveyou@gmail.com>
2024-03-22 17:07:24 +0900

    [Remove] dto 수정에 따라 오류나는 코드 일괄 제거

--- characterlit/src/main/java/com/vamos/characterlit/chat/controller/ChatRoomController.java
+++ characterlit/src/main/java/com/vamos/characterlit/chat/controller/ChatRoomController.java
@@ -18,25 +18,5 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class ChatRoomController {
-//    private final ChatRoomService chatRoomService;
-//
-//    // 유저가 보유한 채팅방 목록 반환
-//    @GetMapping("/chatroomlist/{senderId}")
-//    public ResponseEntity chatRoomList(@PathVariable("senderId") Long senderId) {
-//        return ResponseEntity.ok(chatRoomService.chatRoomList(senderId));
-//    }
-//
-//    // 채팅방 생성 후 생성된 채팅방 링크 반환
-//    @PostMapping("/chatcreate")
-//    public ResponseEntity chatRoomCreate(@RequestBody ChatRoomDTO chatRoomDTO) {
-//        Long roomId = chatRoomService.createChatRoom(chatRoomDTO);
-//
-//        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-//                .path("/{id}")
-//                .buildAndExpand(roomId)
-//                .toUri();
-//
-//        // header 내 로케이션에 uri 반환
-//        return ResponseEntity.created(location).build();
-//    }
+
 }
