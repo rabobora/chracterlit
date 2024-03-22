1b4ccbc7160e554ffe926feff530ff7e4a497711
azin Lee <nuestiloveyou@gmail.com>
2024-03-22 17:07:49 +0900

    [Remove] dto 수정에 따라 세부 컬럼 제거

--- characterlit/src/main/java/com/vamos/characterlit/chat/dto/ChatDTO.java
+++ characterlit/src/main/java/com/vamos/characterlit/chat/dto/ChatDTO.java
@@ -1,19 +1,5 @@
 package com.vamos.characterlit.chat.dto;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Builder
-@Getter
-@NoArgsConstructor(access=AccessLevel.PROTECTED)
-@AllArgsConstructor
-@ToString
 public class ChatDTO { // mongoDB 적용하면 수정될 예정입니다.
-    private Long roomId;
-    private Long senderId;
-    private String content;
+
 }
