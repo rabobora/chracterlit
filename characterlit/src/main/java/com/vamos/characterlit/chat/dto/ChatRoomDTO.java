18dedbfce30f31ad71761ff5f1b63931851c229e
azin Lee <nuestiloveyou@gmail.com>
2024-03-22 17:07:49 +0900

    [Remove] dto 수정에 따라 세부 컬럼 제거

--- characterlit/src/main/java/com/vamos/characterlit/chat/dto/ChatRoomDTO.java
+++ characterlit/src/main/java/com/vamos/characterlit/chat/dto/ChatRoomDTO.java
@@ -1,27 +1,5 @@
 package com.vamos.characterlit.chat.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Builder
-@Getter
-@NoArgsConstructor(access=AccessLevel.PROTECTED)
-@AllArgsConstructor
-@ToString
-@Entity
-@Table(name="chatrooms")
 public class ChatRoomDTO { // 수정중입니다.
-    @Id @GeneratedValue(strategy=GenerationType.AUTO)
-    Long chatroomId;
-    Long sellerId;
-    Long buyerId;
+
 }
