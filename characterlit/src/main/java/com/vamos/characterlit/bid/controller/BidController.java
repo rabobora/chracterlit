871b6b66e472dd4b577b3b9fa01d5c0d93bd852a
최홍준 <wushuchj@gmail.com>
2024-03-24 23:58:22 +0900

    [Feat] 메시지 브로커 통신 기능 구현

--- characterlit/src/main/java/com/vamos/characterlit/bid/controller/BidController.java
+++ characterlit/src/main/java/com/vamos/characterlit/bid/controller/BidController.java
@@ -1,12 +1,17 @@
 package com.vamos.characterlit.bid.controller;
 
+import com.vamos.characterlit.bid.request.BidMessageDTO;
 import com.vamos.characterlit.bid.request.BidRequestDTO;
 import com.vamos.characterlit.bid.service.NowbidService;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/bid")
@@ -14,23 +19,35 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class BidController {
 
-    private final NowbidService nowbidService;
+    private final RabbitTemplate template;
+    private final DirectExchange direct;
 
-    public BidController(NowbidService nowbidService) {
-        this.nowbidService = nowbidService;
+    public BidController(RabbitTemplate template, DirectExchange direct) {
+        this.template = template;
+        this.direct = direct;
     }
 
     @PostMapping("/read/{bidId}")
     public ResponseEntity<?> bid(@PathVariable("bidId") Long bidId,
                                  @RequestBody BidRequestDTO bidRequestDTO,
                                  HttpSession session) {
-        log.info("Received bid request for bidId: {}", bidId);
+        log.info("API Received bid request for bidId: {}", bidId);
 
         try {
-            // 경매 정보를 업데이트하는 비즈니스 로직 호출
-            nowbidService.update(bidId, bidRequestDTO);
+            // rabbitMQ에게 메시지 전달
+            BidMessageDTO bidMessage = BidMessageDTO.builder()
+                    .bidId(bidId)
+                    .userId(bidRequestDTO.getUserId())
+                    .requestBid(bidRequestDTO.getRequestBid())
+                    .bidTime(LocalDateTime.now())
+                    .build();
+            if(bidId%2==0){
+                template.convertAndSend(direct.getName(), "queue_1_routing_key", bidMessage);
+            } else {
+                template.convertAndSend(direct.getName(), "queue_2_routing_key", bidMessage);
+            }
 
-            return new ResponseEntity<>("Success", HttpStatus.OK);
+            return new ResponseEntity<>("Message Add", HttpStatus.OK);
         } catch (Exception e) {
             log.error("Error processing the bid: {}", e.getMessage());
             return new ResponseEntity<>("Error processing the bid: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
--- characterlit/src/main/java/com/vamos/characterlit/bid/service/NowbidService.java
+++ characterlit/src/main/java/com/vamos/characterlit/bid/service/NowbidService.java
@@ -1,14 +1,16 @@
 package com.vamos.characterlit.bid.service;
 
 import com.vamos.characterlit.bid.domain.Bidlogs;
+import com.vamos.characterlit.bid.request.BidMessageDTO;
+import com.vamos.characterlit.items.domain.Items;
+import com.vamos.characterlit.items.repository.ItemRepository;
 import com.vamos.characterlit.sse.domain.BidEvent;
 import com.vamos.characterlit.bid.domain.Nowbid;
 import com.vamos.characterlit.bid.repository.BidlogsRepository;
 import com.vamos.characterlit.bid.repository.NowbidRepository;
-import com.vamos.characterlit.bid.request.BidRequestDTO;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
@@ -16,52 +18,62 @@ import java.util.Optional;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class NowbidService {
     private final NowbidRepository nowbidRepository;
     private final BidlogsRepository bidlogsRepository;
+    private final ItemRepository itemRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
-    @Autowired
-    private ApplicationEventPublisher eventPublisher;
-
-    public NowbidService(NowbidRepository nowbidRepository, BidlogsRepository bidlogsRepository) {
-        this.nowbidRepository = nowbidRepository;
-        this.bidlogsRepository = bidlogsRepository;
-    }
     @Transactional
-    public void update(Long bidId, BidRequestDTO bidRequestDTO) {
-        // bidlogs에 대한 검증 처리 없습니다. 추가 필요.
-        log.info("Bidding process Activated : bidId={}, requestbid={}",bidId, bidRequestDTO.getRequestBid());
-        Optional<Nowbid> nowbidOptional = nowbidRepository.findById(bidId);
-        if(nowbidOptional.isPresent()){
-            Nowbid topbid = nowbidOptional.get();
+    public void update(BidMessageDTO messageDTO) {
+        log.info("Bidding process Activated : bidId={}, requestbid={}",
+                messageDTO.getBidId(), messageDTO.getRequestBid());
 
-            if(topbid.getPresentBid() < bidRequestDTO.getRequestBid()) {
-                topbid.setPresentBid(bidRequestDTO.getRequestBid());
-                Bidlogs bidLog =Bidlogs.builder()
-                        .bidId(bidId)
-                        .requestBid(bidRequestDTO.getRequestBid())
-                        .bidTime(bidRequestDTO.getBidTime())
-                        .build();
+        Items target = itemRepository.findByBidId(messageDTO.getBidId())
+                .orElseThrow(() -> new RuntimeException("Item not found for bidId: " + messageDTO.getBidId()));
+        log.info("Bidding Time : bidTime={}", messageDTO.getBidTime());
+        log.info("Start Time : bidTime={}", target.getStartDate());
+        log.info("End Time : bidTime={}", target.getEndDate());
+          if (target.getStartDate().isAfter(messageDTO.getBidTime())
+                || target.getEndDate().isBefore(messageDTO.getBidTime())) {
+            throw new RuntimeException("Bid time is not within the auction period.");
+        }
+        if (target.getStartBid() > messageDTO.getRequestBid()) {
+            throw new RuntimeException("The bid is lower than the start bid.");
+        }
 
-                nowbidRepository.save(topbid);
-                bidlogsRepository.save(bidLog);
-            } else {
-                throw new RuntimeException("The bid is lower than the current top bid.");
+        Optional<Nowbid> nowbidOptional = nowbidRepository.findById(messageDTO.getBidId());
+        if (nowbidOptional.isPresent()) {
+            Nowbid bidTarget = nowbidOptional.get();
+            if(bidTarget.getPresentBid() >= messageDTO.getRequestBid()){
+                throw new RuntimeException("The bid is lower than the current bid.");
             }
+            bidTarget.setPresentBid(messageDTO.getRequestBid());
+            Bidlogs bidLog = Bidlogs.builder()
+                    .bidId(messageDTO.getBidId())
+                    .userId(messageDTO.getUserId())
+                    .requestBid(messageDTO.getRequestBid())
+                    .bidTime(messageDTO.getBidTime())
+                    .build();
+            nowbidRepository.save(bidTarget);
+            bidlogsRepository.save(bidLog);
         } else {
+            log.info("First Bidding at bidId={}",messageDTO.getBidId());
             Nowbid newBid = new Nowbid();
-            newBid.setBidId(bidId);
-            newBid.setPresentBid(bidRequestDTO.getRequestBid());
-            Bidlogs bidLog =Bidlogs.builder()
-                    .bidId(bidId)
-                    .requestBid(bidRequestDTO.getRequestBid())
-                    .bidTime(bidRequestDTO.getBidTime())
+            newBid.setBidId(messageDTO.getBidId());
+            newBid.setPresentBid(messageDTO.getRequestBid());
+            Bidlogs bidLog = Bidlogs.builder()
+                    .bidId(messageDTO.getBidId())
+                    .userId(messageDTO.getUserId())
+                    .requestBid(messageDTO.getRequestBid())
+                    .bidTime(messageDTO.getBidTime())
                     .build();
             nowbidRepository.save(newBid);
             bidlogsRepository.save(bidLog);
         }
-        log.info("Bidding Event Process Start");
-        BidEvent event = new BidEvent(this, bidId, bidRequestDTO.getUserId(), bidRequestDTO.getRequestBid());
+        log.info("Bidding event process started for bidId={}", messageDTO.getBidId());
+        BidEvent event = new BidEvent(this, messageDTO.getBidId(), messageDTO.getUserId(), messageDTO.getRequestBid());
         log.info(String.valueOf(event));
         eventPublisher.publishEvent(event);
     }
