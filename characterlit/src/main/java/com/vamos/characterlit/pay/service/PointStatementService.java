ff99e531ac12cb4940ccf27986a42ca2bb9eec33
rabobora <kboraim@gmail.com>
2024-03-25 17:54:13 +0900

    [Feat] 추가 포인트 정산

--- characterlit/src/main/java/com/vamos/characterlit/pay/service/PointStatementService.java
+++ characterlit/src/main/java/com/vamos/characterlit/pay/service/PointStatementService.java
@@ -1,13 +1,19 @@
 package com.vamos.characterlit.pay.service;
 
+import com.vamos.characterlit.pay.domain.Point;
 import com.vamos.characterlit.pay.domain.PointStatements;
+import com.vamos.characterlit.pay.repository.PointRepository;
 import com.vamos.characterlit.pay.repository.PointStatementRepository;
 import com.vamos.characterlit.pay.response.StatementResponseDTO;
+import com.vamos.characterlit.user.domain.Users;
 import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 
 @Service
@@ -15,6 +21,8 @@ import java.util.Random;
 public class PointStatementService {
 
     private final PointStatementRepository pointStatementRepository;
+    private final PointRepository pointRepository;
+    private final PointStatementService pointStatementService;
 
     // 포인트 내역 조회
     public List<StatementResponseDTO> pointStatementsList(Long userNumber) {
@@ -46,6 +54,20 @@ public class PointStatementService {
             }
         }
         return sb.toString();
+    }
+
+    // 포인트 홀딩하기
+    @Scheduled(cron = "0 0 0 * * *")
+    public void pointHolding(){
+        LocalDate localDate = LocalDate.now().minusDays(8);
+        List<PointStatements> pointList = pointStatementRepository.findByStatementDate(localDate);
+
+        for(PointStatements state : pointList){
+            state.setPointStatus(5);
+            Point point = pointRepository.findByuserNumber(state.getUserNumber());
+            int usablePoint = point.getUsablePoint();
+            point.setUsablePoint(usablePoint+state.getPoint());
+        }
     }
 }
 
