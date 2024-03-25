22401abfad11859e8b1b81047af99766feffc5a8
azin Lee <nuestiloveyou@gmail.com>
2024-03-25 15:06:08 +0900

    [Add] mongoDB Sechat 관련 rvice 작 추가

---
+++ characterlit/src/main/java/com/vamos/characterlit/chat/service/mongoDBChatService.java
@@ -0,0 +1,21 @@
+package com.vamos.characterlit.chat.service;
+
+import com.vamos.characterlit.chat.dto.mongoDBChatDTO;
+import com.vamos.characterlit.chat.repository.mongoDBChatRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class mongoDBChatService {
+    private final mongoDBChatRepository mongoDBchatRepository;
+
+    public List<mongoDBChatDTO> listMongoDBChat(Long chatroomId){
+        return mongoDBchatRepository.findByChatroomId(chatroomId);
+    }
+    public void saveMongoDBChat(List<mongoDBChatDTO> mongoDBchatDTOList){
+        mongoDBchatRepository.saveAll(mongoDBchatDTOList);
+    }
+}
