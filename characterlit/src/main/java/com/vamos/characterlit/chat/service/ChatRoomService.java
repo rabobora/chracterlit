76c4ea61a6c70f61256eb4baa8180ec67aee9ad9
azin Lee <nuestiloveyou@gmail.com>
2024-03-25 15:16:40 +0900

    [Add] 채팅방 생성, 채팅방 리스팅 method 추가

--- characterlit/src/main/java/com/vamos/characterlit/chat/service/ChatRoomService.java
+++ characterlit/src/main/java/com/vamos/characterlit/chat/service/ChatRoomService.java
@@ -1,11 +1,23 @@
 package com.vamos.characterlit.chat.service;
 
+import com.vamos.characterlit.chat.dto.ChatRoomDTO;
+import com.vamos.characterlit.chat.repository.ChatRoomRepository;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
+    private final ChatRoomRepository chatRoomRepository;
 
+    public Long createChatRoom(ChatRoomDTO chatRoomDTO){
+        return chatRoomRepository.save(chatRoomDTO).getChatroomId();
+    }
+
+    public List<ChatRoomDTO> listChatRoom(Long userId){
+        return chatRoomRepository.findByBuyerIdOrSellerId(userId, userId);
+    }
 }
