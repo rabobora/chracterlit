fa6b3eb6be7473e95b2f360c36105a6afe29342d
azin Lee <nuestiloveyou@gmail.com>
2024-03-25 15:13:11 +0900

    [Add] 채팅방 생성, 사용자의 채팅방 목록 조회 method 추가

--- characterlit/src/main/java/com/vamos/characterlit/chat/controller/ChatRoomController.java
+++ characterlit/src/main/java/com/vamos/characterlit/chat/controller/ChatRoomController.java
@@ -2,6 +2,8 @@ package com.vamos.characterlit.chat.controller;
 
 import java.net.URI;
 
+import com.vamos.characterlit.chat.dto.mongoDBChatRoomDTO;
+import com.vamos.characterlit.chat.service.mongoDBChatRoomService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,5 +20,30 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class ChatRoomController {
+    private final ChatRoomService chatRoomService;
+    private final mongoDBChatRoomService mongoDBchatRoomService;
 
+    // 채팅방 생성
+    @PostMapping("/api/chatroom/create")
+    ResponseEntity createChatRoom(@RequestBody ChatRoomDTO chatRoomDTO){
+        System.out.println(chatRoomDTO);
+        Long chatroomId = chatRoomService.createChatRoom(chatRoomDTO);
+
+        // mongoDB에도 넣어주기
+        mongoDBchatRoomService.createMongoDBChatRoom(chatroomId);
+
+        // http://localhost:8080/api/chatroom/create/생성된채팅방id
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest() // 사용자 요청 uri
+                .path("/{id}")
+                .buildAndExpand(chatroomId) // 새로 설정한 id 값. 위 path에 지정됨
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    // 사용자 채팅방 목록 조회
+    @GetMapping("/api/chatroomlist/{userId}")
+    public ResponseEntity chatRoomList(@PathVariable("userId") Long userId) {
+        return ResponseEntity.ok(chatRoomService.listChatRoom(userId));
+    }
 }
