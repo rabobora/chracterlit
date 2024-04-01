129dccb05e2faca0f7d7724b82e140ac4f8a75bd
권근열 <yeol3011@naver.com>
2024-04-01 15:53:07 +0900

    [feat] new

--- characterlit/src/main/java/com/vamos/characterlit/chat/repository/mongoDBChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vamos.characterlit.chat.repository;
-
-import com.vamos.characterlit.chat.dto.mongoDBChatRoomDTO;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface mongoDBChatRoomRepository extends MongoRepository<mongoDBChatRoomDTO, Long> {
-}
